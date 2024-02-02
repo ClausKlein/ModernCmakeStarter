454f88738825f1af80fe72c58c48246389cb8d77
ClausKlein <claus.klein@arcormail.de>
2024-02-02 13:14:05 +0100

    make format

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -7,15 +7,15 @@ using greeter::Greeter;
 Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
 
 std::string Greeter::greet(LanguageCode lang) const {
-  switch (lang) {
-    default:
-    case LanguageCode::EN:
-      return fmt::format("Hello, {}!", name);
-    case LanguageCode::DE:
-      return fmt::format("Hallo {}!", name);
-    case LanguageCode::ES:
-      return fmt::format("¡Hola {}!", name);
-    case LanguageCode::FR:
-      return fmt::format("Bonjour {}!", name);
-  }
+    switch (lang) {
+        default:
+        case LanguageCode::EN:
+            return fmt::format("Hello, {}!", name);
+        case LanguageCode::DE:
+            return fmt::format("Hallo {}!", name);
+        case LanguageCode::ES:
+            return fmt::format("¡Hola {}!", name);
+        case LanguageCode::FR:
+            return fmt::format("Bonjour {}!", name);
+    }
 }
